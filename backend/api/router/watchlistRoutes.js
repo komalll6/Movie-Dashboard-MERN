388ebc49775acbb06786b7142891ea9b05e006ee
komalll6 <komalkalra698@gmail.com>
2026-08-14 19:00:57 +0530

    remove unwanted code from backend files

--- backend/api/router/watchlistRoutes.js
+++ backend/api/router/watchlistRoutes.js
@@ -1,12 +1,3 @@
-// import express from "express";
-// const router = express.Router();
-
-// router.post("/add", (req, res) => {
-//   res.json({ message: "Item added to watchlist", item: req.body });
-// });
-
-// export default router;
-
 import express from "express";
 const router = express.Router();
 
