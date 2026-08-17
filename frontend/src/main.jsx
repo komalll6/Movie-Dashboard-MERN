d0b7b644409afb157490ff056241922206ea0052
komalll6 <komalkalra698@gmail.com>
2026-08-17 13:38:47 +0530

    commit with frontend files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { AppProvider } from './context/AppContext'; // 👈 Check Provider import
+import { AppProvider } from './context/AuthContext'; // 👈 AuthContext se import kiya gaya hai
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AppProvider> {/* 👈 Ensure AppProvider wraps App */}
+      <AppProvider>
         <App />
       </AppProvider>
     </BrowserRouter>
